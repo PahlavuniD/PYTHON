d1d0a385ea24f54c8b4ca3fa386df4a9d068b35c
PahlavuniD <MagMemoirs@MacBook-Pro-Askanaz.local>
2025-04-19 18:33:39 +0300

    initial commit

---
+++ Hello_Python.py
@@ -0,0 +1,46 @@
+import os
+os.system('clear') # чистит терминал
+# comments
+
+'''
+DATA TYPES
+Strings
+Numbers
+Lists
+Tuples неизменный список, быстрее
+Dictionaries
+Boolean
+
+'''
+##################################
+full_name = "David Pahlavuni"
+print(full_name)
+full_name = "Pahlavuni" # значени переназначается
+full_name = "" # пустая строчка
+name = "David"
+full_name = name 
+
+аge = 41 # numbers
+age = "41" # strings
+
+names1 ["David", "Pahlavuni"] # lists
+print(names[1])
+
+names2 ("David", "Pahlavuni") # tuples
+print(names[0])
+
+fav_names {
+	"David" : "vegetarian food",
+	"Somebody" : "everything else"
+} # первое слово key : второе value
+
+print(fav_names["David"]) # выведет veg food
+
+names = True #boolean 
+# возвращает True или False
+# если что-то существет - True
+######################################
+Version Control with Git and Github
+#	###################################
+
+
